a49eae8368a169dbc5f2ce83209b35949078bdd1
AlexSB664 <alexskullsoft@gmail.com>
2019-07-12 16:24:18 -0700

    Estructura de Proyecto

---
+++ app/Http/Controllers/PrincipalController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+
+
+class PrincipalController extends Controller
+{
+    public function index(){
+    	return view('login');
+    }
+    public function pruebaE(){
+    	$encrypted = Crypt::encryptString('Hello world.');
+    	$decrypted = Crypt::decryptString($encrypted);
+    	return $encrypted.$decrypted;
+    }
+    
+}
